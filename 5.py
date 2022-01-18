d315cdda1ada0128febae49b6b94538425b6a5b6
Ju4ara5 <90102282+Ju4ara5@users.noreply.github.com>
2022-01-18 17:21:58 +0200

    5

--- 5.py
+++ 5.py
@@ -1,20 +1,8 @@
-#  додетать!!!
-
 """2520 - самое маленькое число, которое делится без остатка на все числа от 1 до 10.
    Какое самое маленькое число делится нацело на все числа от 1 до 20?
    """
-#  scm = smallest_common_multiple - наименьшее общее кратное
-#  gcd = greatest_common_divisor - наибольший общий делитель
-#  scm( a ,   b ) = a ⋅ b : (a, b)=a·b:gcd( a ,   b )
 
-# import math
-# print(math.gcd(28, 64))
+import numpy as np
 
-# gcd:
-try:
-    from math import gcd
-except ImportError:
-    from fractions import gcd
-from functools import reduce
-
-print(reduce(gcd, [36, 12, 144, 18]))
+numbers = [i for i in range(1, 21)]
+print(np.lcm.reduce(numbers))
